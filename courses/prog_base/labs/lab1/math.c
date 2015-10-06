207cb0f7c89184a89f69330417de6998e43217e3
samokhvalenko <ctac180898@gmail.com>
2015-10-06 20:35:47 +0300

    1

---
+++ courses/prog_base/labs/lab1/math.c
@@ -0,0 +1,15 @@
+#include <math.h>
+
+double calc(double x, double y, double z) {
+    double a;
+    if (x-y == 0 || x+y == 0 || (cos(y)/sin(x)+1) == 0 )
+    {
+        return NAN;
+    }
+    else
+    {
+        a = pow(x, y + 1) / pow(x - y, 1 / z) + y / ( 4 * (x + y)) + (sqrt(fabs(cos(y)/sin(x))+1) );
+    }
+return a;
+
+}

01e9874977f342ee2745117f6cef742b486f4a2f
samokhvalenko <ctac180898@gmail.com>
2016-03-30 17:27:29 +0300

    Menu commit 2
    This is my menu_for_game v 2.0

--- courses/prog_base_3/project/menu.cpp
+++ courses/prog_base_3/project/menu.cpp
@@ -4,10 +4,14 @@ using namespace sf;
 
 void menu(RenderWindow &window){
 
-    Texture menuTexture1, menuTexture2, menuTexture3, menuBackground;
+    Texture menuTexture1, menuTexture2, menuTexture3, menuTexture1_2, menuTexture2_2, menuTexture3_2, menuBackground;
     menuTexture1.loadFromFile("new_game.png");
 	menuTexture2.loadFromFile("load_game.png");
 	menuTexture3.loadFromFile("exit.png");
+
+	menuTexture1_2.loadFromFile("new_game2.png");
+	menuTexture2_2.loadFromFile("load_game2.png");
+	menuTexture3_2.loadFromFile("exit2.png");
 	menuBackground.loadFromFile("menu_background.jpg");
 	Sprite menu1(menuTexture1), menu2(menuTexture2), menu3(menuTexture3), menuBg(menuBackground);
 	bool isMenu = 1;
@@ -19,22 +23,37 @@ void menu(RenderWindow &window){
 
 	while (isMenu)
 	{
+	    menu1.setTexture(menuTexture1);
+	    menu2.setTexture(menuTexture2);
+	    menu3.setTexture(menuTexture3);
+
 		menu1.setColor(Color::White);
 		menu2.setColor(Color::White);
 		menu3.setColor(Color::White);
 		menuNum = 0;
 		window.clear(Color(129, 181, 221));
 
-		if (IntRect(130, 50, 200, 30).contains(Mouse::getPosition(window))) { menu1.setColor(Color(19, 0, 255)); menuNum = 1; }
-		if (IntRect(130, 100, 150, 30).contains(Mouse::getPosition(window))) { menu2.setColor(Color(19, 0, 255)); menuNum = 2; }
-		if (IntRect(130, 150, 45, 30).contains(Mouse::getPosition(window))) { menu3.setColor(Color(19, 0, 255)); menuNum = 3; }
+		if (IntRect(135, 40, 220, 30).contains(Mouse::getPosition(window))){
+            menuNum = 1;
+            menu1.setTexture(menuTexture1_2);
+        }
+		if (IntRect(130, 100, 150, 30).contains(Mouse::getPosition(window))){
+            menuNum = 2;
+            menu2.setTexture(menuTexture2_2);
+        }
+		if (IntRect(130, 150, 45, 30).contains(Mouse::getPosition(window))){
+            menuNum = 3;
+            menu3.setTexture(menuTexture3_2);
+        }
 
 		if (Mouse::isButtonPressed(Mouse::Left))
 		{
 			if (menuNum == 1)
-                //ride(window);
+                window.close();
+                isMenu = false;
 			if (menuNum == 2) {
-                //add_way(window);
+                window.close();
+                isMenu = false;
             }
 			if (menuNum == 3)  {
 			     window.close();
